bb62ace1048dc7d370e379dfb2ce0f8ea40d4f6a
investigoonline <appr@investigoonline.com>
2026-08-10 12:39:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const judges = [
   },
   {
     name: 'Anuj Shah',
-    title: 'Business Leader',
+    title: 'Successful Entrepreneur, Business Leader',
     bio: 'Anuj Shah, a successful entrepreneur and business leader with extensive experience building and scaling businesses. Having successfully managed large logistics and distribution network operations, Anuj brings valuable insights into leadership, growth strategy, and operational excellence. We are honored to have him share his perspective with our community of investors, founders, and business leaders.',
     img: '/anuj1.jpeg',
   },
